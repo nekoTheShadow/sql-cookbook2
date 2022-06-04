d5ae46fc6776e23cd9f242968f76b966c3973e60
Nakamura Hajime <h.nakamura0903@gmail.com>
2022-06-05 02:05:19 +0900

    レシピ14.3 OracleのMODEL句を使って結果セットを転置する

---
+++ 14-03.sql
@@ -0,0 +1,17 @@
+SELECT MAX(d10) AS d10, MAX(d20) AS d20, MAX(d30) AS d30
+FROM (
+  SELECT *
+  FROM (
+              SELECT 10 AS deptno, 3 AS cnt FROM DUAL
+    UNION ALL SELECT 20 AS deptno, 5 AS cnt FROM DUAL
+    UNION ALL SELECT 30 AS deptno, 6 AS cnt FROM DUAL
+  ) T
+  MODEL 
+  DIMENSION BY (deptno d)
+  MEASURES (deptno, cnt d10, cnt d20, cnt d30)
+  RULES (
+    d10[any] = CASE WHEN deptno[cv()] = 10 THEN d10[cv()] ELSE 0 END,
+    d20[any] = CASE WHEN deptno[cv()] = 20 THEN d20[cv()] ELSE 0 END,
+    d30[any] = CASE WHEN deptno[cv()] = 30 THEN d30[cv()] ELSE 0 END
+  )
+) S
