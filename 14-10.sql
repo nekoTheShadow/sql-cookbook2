b4ee1222e0c49d4015babb22bc2101ebea015d0a
Nakamura Hajime <h.nakamura0903@gmail.com>
2022-06-05 02:06:01 +0900

    レシピ14.10 Oracleでスカラサブクエリを複合サブクエリに変換する

---
+++ 14-10.sql
@@ -0,0 +1,23 @@
+/**
+CREATE TABLE dept (
+  deptno INT,
+  dname  VARCHAR(14),
+  loc    VARCHAR(13)
+);
+INSERT INTO dept (deptno, dname, loc) VALUES (10, 'ACCOUNTING', 'NEW YORK');
+INSERT INTO dept (deptno, dname, loc) VALUES (20, 'RESEARCH'  , 'DALLAS'  );
+INSERT INTO dept (deptno, dname, loc) VALUES (30, 'SALES'     , 'CHICAGO' );
+INSERT INTO dept (deptno, dname, loc) VALUES (40, 'OPERATIONS', 'BOSTON'  );
+
+CREATE TYPE generic_obj AS OBJECT (
+  val1 varchar2(10),
+  val2 varchar2(10),
+  val3 date
+)
+**/
+
+SELECT X.deptno, X.ename, X.sal, X.multival.val1 AS dname, X.multival.val2 AS loc, X.multival.val3 AS today
+FROM (
+  SELECT  E.deptno, E.ename, E.sal, (SELECT generic_obj(D.dname, D.loc, sysdate) FROM dept D WHERE E.deptno = D.deptno) multival
+  FROM emp E
+) X
