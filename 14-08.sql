2a215cee86c06ffe5e5b4ab384537c73719f00a5
Nakamura Hajime <h.nakamura0903@gmail.com>
2022-06-05 02:05:50 +0900

    レシピ14.8 ランク付けされた結果セットをピボットする

---
+++ 14-08.sql
@@ -0,0 +1,20 @@
+WITH 
+A AS (
+  SELECT ename, sal, DENSE_RANK() OVER (ORDER BY sal DESC) DR
+  FROM emp
+),
+B AS (
+  SELECT ename, sal, CASE WHEN DR <= 3 THEN 1 WHEN DR <= 6 THEN 2 ELSE 3 END grp
+  FROM A
+), 
+C AS (
+  SELECT ename, sal, grp, ROW_NUMBER() OVER (PARTITION BY grp ORDER BY sal DESC) grp_rnk
+  FROM B
+)
+SELECT
+  MAX(CASE WHEN grp = 1 THEN ename || ' (' || sal || ')' END) AS top_3,
+  MAX(CASE WHEN grp = 2 THEN ename || ' (' || sal || ')' END) AS next_3,
+  MAX(CASE WHEN grp = 3 THEN ename || ' (' || sal || ')' END) AS rest
+FROM C
+GROUP BY grp_rnk
+ORDER BY grp_rnk
